83132abe4d0e3137ef94c44f1c6aa644c2ff0fcc
Andrey Khomyakov <and-hom@yandex-team.ru>
2016-11-27 13:41:26 +0300

    Work started

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	log "github.com/Sirupsen/logrus"
+	"github.com/jasonlvhit/gocron"
+)
+
+
+
+func main() {
+	//log.Infof("Starting http server on %s", httpStr)
+	//http.HandleFunc("/", handler)
+	//err := http.ListenAndServe(httpStr, nil)
+	//if err != nil {
+	//	log.Fatalf("Can not start server: %v", err)
+	//}
+}
