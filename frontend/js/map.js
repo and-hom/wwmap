50cb96aa2e8702d511348b9af7edea861f7a987b
Андрей Хомяков <and.hom@gmail.com>
2018-04-17 23:39:24 +0300

    category label fix

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -22,7 +22,7 @@
                 '<h3 class="popover-title">'+
                 '[if properties.link]<a target="_blank" href="$[properties.link]">$[properties.title]</a>[else]$[properties.title][endif]</h3>'+
                     '<div class="popover-content">' +
-                    '<div>Категория сложности: $[properties.category]</div>' +
+                    '<div>Категория сложности: [if properties.category=="0"]не определена[else]$[properties.category][endif]</div>' +
                     '<div>$[properties.short_desc]</div>' +
                     '<a id="report_$[properties.link]" href="button">Сообщить о неточном местоположении<a/>' +
                     '</div>'
