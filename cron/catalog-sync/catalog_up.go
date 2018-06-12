576d033fa12626c02c4cee4cfc4b0763117eb253
Андрей Хомяков <and.hom@gmail.com>
2018-06-12 21:50:57 +0300

    some refactoring

---
+++ cron/catalog-sync/catalog_up.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"github.com/and-hom/wwmap/lib/dao"
+	"strconv"
+	"github.com/and-hom/wwmap/cron/catalog-sync/huskytm"
+	log "github.com/Sirupsen/logrus"
+)
+
+func (this App) DoWriteCatalog() {
+	log.Info("Create missing ww passports")
+	catalogConnector := this.getCachedCatalogConnector()
+
+	wwpts, err := this.WhiteWaterDao.ListWithPath()
+	if err != nil {
+		this.Fatalf(err, "Can not connect list ww points")
+	}
+
+	for i := 0; i < len(wwpts); i++ {
+		point := &wwpts[i]
+		log.Debug("Write point ", point.Title)
+		exists, err := catalogConnector.Exists(point.Path)
+		if err != nil {
+			this.Fatalf(err, "Can not check page exists")
+		}
+		if !exists {
+			log.Debug("Not exists - create")
+			imgs, err := this.ImgDao.List(point.Id, 1)
+			if err != nil {
+				this.Fatalf(err, "Can not get images for ww point %d", point.Id)
+			}
+			imgId := -1
+			if len(imgs)>0 {
+				imgId64, err := strconv.ParseInt(imgs[0].RemoteId,10, 32)
+				if err != nil {
+					this.Fatalf(err, "Can not parse img remote id", imgs[0].RemoteId)
+				}
+				imgId = int(imgId64)
+			}
+			this.createCatalogEntry(point, imgId)
+		} else {
+			log.Debugf("Point %s exists", point.Path)
+		}
+	}
+}
+
+func (this App) createCatalogEntry(p *dao.WhiteWaterPointWithPath, imgId int) {
+	parent_id, err := this.mkdirRecursiveQuietly(parent(p.Path))
+	if err != nil {
+		this.Fatalf(err, "Can not create directories")
+	}
+	catalogConnector := this.getCachedCatalogConnector()
+	err = catalogConnector.Create(p.WhiteWaterPoint, imgId, parent_id, []dao.Img{})
+	if err != nil {
+		this.Fatalf(err, "Can not create passport")
+	}
+}
+
+func parent(path []string) []string {
+	if len(path) == 0 {
+		log.Fatal("Can not create parent of empty path")
+	}
+	return path[:len(path) - 1]
+}
+
+func (this *App) mkdirRecursiveQuietly(p []string) (int, error) {
+	if len(p) == 0 {
+		return huskytm.ROOT_PAGE, nil
+	}
+	catalogConnector := this.getCachedCatalogConnector()
+	pp := parent(p)
+
+	parent, err := this.mkdirRecursiveQuietly(pp)
+	if err != nil {
+		return 0, err
+	}
+
+	exists, err := catalogConnector.Exists(p)
+	if err != nil {
+		return 0, err
+	}
+	title := p[len(p) - 1]
+	if !exists {
+		log.Debug("Mk dir ", p)
+		return catalogConnector.CreatePage(title, parent)
+	} else {
+		log.Debug("Dir exists ", p)
+		return catalogConnector.GetId(title, parent)
+	}
+}
--- cron/catalog-sync/report.go
+++ cron/catalog-sync/report.go
@@ -1,6 +1,153 @@
 package main
 
-type ImportExportReport struct {
-	ReportsFound int
-	ReportsImported int
+import (
+	"github.com/and-hom/wwmap/lib/dao"
+	"strings"
+	log "github.com/Sirupsen/logrus"
+	"io"
+	"github.com/and-hom/wwmap/cron/catalog-sync/huskytm"
+	"time"
+	"github.com/and-hom/wwmap/cron/catalog-sync/common"
+	"fmt"
+	"html/template"
+	"github.com/and-hom/wwmap/lib/mail"
+)
+
+func (this App) DoSyncReports() {
+	lastId, err := this.VoyageReportDao.GetLastId(huskytm.SOURCE)
+	if err != nil {
+		this.Fatalf(err, "Can not connect get last report id")
+	}
+	lastReportIdStr := lastId.(time.Time).Format(huskytm.TIME_FORMAT)
+	log.Infof("Get and store reports since %s", lastReportIdStr)
+
+	reportProvider, err := huskytm.GetReportProvider(this.Configuration.Login, this.Configuration.Password)
+	if err != nil {
+		this.Fatalf(err, "Can not connect to source")
+	}
+	defer reportProvider.Close()
+
+	reports, next, err := reportProvider.ReportsSince(lastReportIdStr)
+	if err != nil {
+		log.Fatal(err, "Can not get posts")
+	}
+	if len(reports) == 0 {
+		next = lastReportIdStr
+	}
+
+	reports, err = this.VoyageReportDao.UpsertVoyageReports(reports...)
+	if err != nil {
+		this.Fatalf(err, "Can not store reports: %v", reports)
+	}
+
+	log.Infof("%d reports are successfully stored. Next id is %s\n", len(reports), next)
+
+	log.Info("Try to connect reports with known rivers")
+
+	for i, report := range reports {
+		log.Infof("Tags are: %v", report.Tags)
+		rivers, err := this.RiverDao.FindTitles(report.Tags)
+		if err != nil {
+			log.Fatal("Can not find rivers for tags", report.Tags, err)
+		}
+		log.Info(rivers)
+		for _, river := range rivers {
+			err := this.VoyageReportDao.AssociateWithRiver(report.Id, river.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		reports[i].Rivers = rivers
+	}
+
+	for _, report := range reports {
+		this.findMatchAndStoreImages(report, reportProvider)
+	}
+}
+
+func (this App) findMatchAndStoreImages(report dao.VoyageReport, reportProvider common.ReportProvider) {
+	log.Infof("Find images for report %d: %s %s", report.Id, report.RemoteId, report.Title)
+	imgs, err := reportProvider.Images(report.RemoteId)
+	if err != nil {
+		this.Fatalf(err, "Can not load images for report %d", report.Id)
+	}
+	fmt.Printf("%d images found\n", len(imgs))
+
+	log.Infof("Bind images to ww spots for report %d", report.Id)
+	matchedImgs := []dao.Img{}
+	candidates := this.matchImgsToWhiteWaterPoints(report, imgs)
+
+	for _, imgToWwpts := range candidates {
+		if len(imgToWwpts.Wwpts) == 1 {
+			imgToWwpts.Img.WwId = imgToWwpts.Wwpts[0].Id
+			matchedImgs = append(matchedImgs, imgToWwpts.Img)
+		} else if len(imgToWwpts.Wwpts) > 1 {
+			log.Warn("More then one white water point for img ", imgToWwpts.Img.RemoteId)
+		}
+	}
+
+	log.Infof("Store %d images for report %d", len(matchedImgs), report.Id)
+	_, err = this.ImgDao.Upsert(matchedImgs...)
+	if err != nil {
+		this.Fatalf(err, "Can not upsert images for report %d", report.Id, )
+	}
+}
+
+type ImgWwPoints struct {
+	Img   dao.Img
+	Wwpts []dao.WhiteWaterPointWithRiverTitle
+}
+
+func (this App) matchImgsToWhiteWaterPoints(report dao.VoyageReport, imgs []dao.Img) map[string]ImgWwPoints {
+	candidates := make(map[string]ImgWwPoints)
+	for _, img := range imgs {
+		for _, river := range report.Rivers {
+			wwpts, err := this.WhiteWaterDao.ListByRiver(river.Id)
+			if err != nil {
+				this.Fatalf(err, "Can not list white water spots for river %d", river.Id)
+			}
+			for _, wwpt := range wwpts {
+				for _, label := range img.LabelsForSearch {
+					if strings.Contains(forCompare(label), forCompare(wwpt.Title)) {
+						fmt.Println("Found: ", label)
+						rec, found := candidates[img.RemoteId]
+						if !found {
+							rec = ImgWwPoints{
+								Img:img,
+								Wwpts:[]dao.WhiteWaterPointWithRiverTitle{},
+							}
+						}
+						rec.Wwpts = append(rec.Wwpts, wwpt)
+						candidates[img.RemoteId] = rec
+					}
+				}
+			}
+		}
+	}
+	return candidates
+}
+
+func forCompare(s string) string {
+	return strings.Replace(strings.Replace(strings.ToLower(s), "ё", "e", -1), "-", " ", -1)
+}
+
+func (this App) Fatalf(err error, pattern string, args ...interface{}) {
+	this.Report(err)
+	log.Fatalf(pattern + ": " + err.Error(), args)
+}
+
+func (this App) Report(err error) {
+	templateData, err := emailTemplateBytes()
+	if err != nil {
+		log.Fatal("Can not load email template:\t", err)
+	}
+
+	tmpl, err := template.New("report-email").Parse(string(templateData))
+	if err != nil {
+		log.Fatal("Can not compile email template:\t", err)
+	}
+
+	err = mail.SendMail(this.Notifications.EmailSender, this.Notifications.EmailRecipients, this.Notifications.ImportExportEmailSubject, func(w io.Writer) error {
+		return tmpl.Execute(w, *this.stat)
+	})
 }
