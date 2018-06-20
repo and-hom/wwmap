f9ea4f50b0772797c9178013ecb20c00ef8d8cd7
Андрей Хомяков <and.hom@gmail.com>
2018-06-21 02:33:34 +0300

    Work on editor: use real content on page #71

---
+++ frontend-2/js/edit.js
@@ -0,0 +1,35 @@
+const apiBase = "http://localhost:7007";
+
+function doGetJsonSync(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    if (xhr.status == 200) {
+        return JSON.parse(xhr.response)
+    }
+    return null
+}
+
+function getCountries() {
+    return doGetJsonSync(apiBase + "/country")
+}
+
+function getRegions(countryId) {
+    return doGetJsonSync(apiBase + "/country/" + countryId + "/region")
+}
+
+function getRiversByCountry(countryId) {
+    return doGetJsonSync(apiBase + "/country/" + countryId + "/river")
+}
+
+function getRiversByRegion(countryId, regionId) {
+    return doGetJsonSync(apiBase + "/country/" + countryId + "/region/" + regionId + "/river")
+}
+
+function getReports(riverId) {
+    return doGetJsonSync(apiBase + "/river/" + riverId + "/reports")
+}
+
+function getSpots(riverId) {
+    return doGetJsonSync(apiBase + "/river/" + riverId + "/spots")
+}
