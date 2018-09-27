2985e2485aaf1ff9b55ce6589bbc46ca791a0f9c
Андрей Хомяков <and.hom@gmail.com>
2018-09-27 13:34:10 +0300

    User unique index by provider and id, not for id only #107

--- db/0035_stage_035.down.sql
+++ db/0035_stage_035.down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE "user" DROP COLUMN auth_provider;
 ALTER TABLE "user" RENAME COLUMN ext_id TO yandex_id;
+
+DROP INDEX user_yandex_id;
+CREATE INDEX user_yandex_id ON "user"(yandex_id);
--- db/0035_stage_035.up.sql
+++ db/0035_stage_035.up.sql
@@ -4,3 +4,6 @@ ALTER TABLE "user" ALTER COLUMN auth_provider SET NOT NULL;
 
 ALTER TABLE "user" RENAME COLUMN yandex_id TO ext_id;
 
+DROP INDEX user_yandex_id;
+CREATE INDEX user_yandex_id ON "user"(auth_provider, ext_id);
+
