7abf44560f5b872d850f0ff0d16e567361dd9b93
Андрей Хомяков <and.hom@gmail.com>
2018-09-28 22:20:39 +0300

    About page

--- frontend/components/page.vue
+++ frontend/components/page.vue
@@ -2,8 +2,12 @@
 <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="navbar-nav mr-auto">
-            <li :class="pageClass(page.href)" v-if="showMenuItem(page)" v-for="page in pages">
-                <a class="nav-link" :href="pageLink(page.href)">{{pageTitle(page)}}</a>
+            <li :class="itemClass(page)" v-if="showMenuItem(page)" v-for="page in pages">
+                <a v-if="page.sub==null" class="nav-link" :href="pageLink(page.href)">{{pageTitle(page)}}</a>
+                <a v-else class="nav-link dropdown-toggle" :id="page.id" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{page.title}}</a>
+                <div v-if="page.sub" class="dropdown-menu" :aria-labelledby="page.id">
+                  <a v-for="sub in page.sub" v-if="showMenuItem(sub)" :class="subItemClass(sub)" :href="pageLink(sub.href)">{{sub.title}}</a>
+                </div>
             </li>
         </ul>
         <auth></auth>
@@ -48,9 +52,24 @@
                         allow: ['ADMIN'],
                     },
                     {
-                        href: "docs.htm",
-                        title: "Прочитай меня",
+                        href: "about.htm",
+                        title: "О проекте",
+                        allow: ['USER', 'ANONYMOUS'],
+                    },
+                    {
+                        id: "1",
+                        title: "Информация",
                         allow: ['EDITOR', 'ADMIN'],
+                        sub: [
+                            {
+                                href: "docs.htm",
+                                title: "Прочитай меня",
+                            },
+                            {
+                                href: "about.htm",
+                                title: "О проекте",
+                            },
+                        ]
                     },
                 ],
                 showMenuItem: function(page) {
@@ -80,11 +99,20 @@
                     }
                     return link
                 },
-                pageClass: function(link) {
-                    if (link==this.link) {
-                        return "nav-item"
+                itemClass: function(page) {
+                    if (page.href==this.link) {
+                        return "nav-item active"
                     }
-                    return "nav-item active"
+                    if (page.sub) {
+                        return "nav-item dropdown"
+                    }
+                    return "nav-item"
+                },
+                subItemClass: function(sub) {
+                    if (sub.href==this.link) {
+                        return "dropdown-item active"
+                    }
+                    return "dropdown-item"
                 },
             }
         },
