f92b6e77d3aea2152c2d85f9b9379420114bb7af
Андрей Хомяков <and.hom@gmail.com>
2018-12-04 23:39:28 +0300

    Run separate cron processes for tlib, pdf and huskytm

--- cron/debian/wwmap-cron.cron.d
+++ cron/debian/wwmap-cron.cron.d
@@ -1,5 +1,7 @@
 */1 * * * * wwmap wwmap-notifier
 0 0 * * 7 wwmap wwmap-backup
-12 * * * * wwmap wwmap-catalog-sync
+0 1 * * * wwmap wwmap-catalog-sync -source pdf
+0 0 * * * wwmap wwmap-catalog-sync -source tlib
+0 0 * * 6 wwmap wwmap-catalog-sync -source huskytm
 0 0 1 * * wwmap wwmap-db-clean
 0 23 * * * wwmap wwmap-spot-sort
