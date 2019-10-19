53999dfdfe029815b0304d96499233d32d4c1f18
Andrey Khomyakov <and.hom@gmail.com>
2019-10-19 14:25:44 +0300

    #209 osm_id=>id references table SQL fix

--- data/rivers_parser/osm_to_id.sql
+++ data/rivers_parser/osm_to_id.sql
@@ -1,4 +1,4 @@
-INSERT INTO waterway_ref(id,ref_id)
-    SELECT w1.id AS id, w2.id AS ref_id FROM waterway w1
+INSERT INTO waterway_ref(id,ref_id,cross_point)
+    SELECT w1.id AS id, w2.id AS ref_id, lnk.cross_point AS cross_point FROM waterway w1
         INNER JOIN waterway_osm_ref lnk ON w1.osm_id=lnk.id
         INNER JOIN waterway w2 ON lnk.ref_id=w2.osm_id;
