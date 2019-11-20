ba0930e31fb41dab4e5b902b1277ea8059967443
Andrey Khomyakov <and.hom@gmail.com>
2019-11-20 23:24:19 +0300

    #250 Create empty webpack project for backend

---
+++ frontend/editor-js-sources/webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const {resolve} = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const DEVELOPMENT = 'development';
+
+module.exports = env => {
+    let appEnv = DEVELOPMENT;
+    if (env && env.APP_ENV) {
+        appEnv = env.APP_ENV;
+    }
+
+    console.log(`Build ${appEnv} environment`);
+
+    return {
+        mode: appEnv == DEVELOPMENT ? 'development' : 'production',
+        context: __dirname,
+        devtool: "source-map",
+        entry: ["./main.js",],
+        output: {
+            path: __dirname + "/../js-editor",
+            filename: "editor.v2.js",
+            publicPath: './js-editor/',
+            libraryTarget: 'var',
+            library: 'wwmap'
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+                ignoreOrder: false,
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /.*?\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /config\.js$/,
+                    loader: 'file-replace-loader',
+                    options: {
+                        condition: appEnv !== DEVELOPMENT,
+                        replacement: resolve('./config.production.js'),
+                        async: true,
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]'
+                    }
+                }
+            ]
+        },
+    }
+};
