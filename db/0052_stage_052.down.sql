2462d6bd9da74e61ceaa6431eb71348ed02d4394
Andrey Khomyakov <and.hom@gmail.com>
2019-12-01 22:56:53 +0300

    #264 Table structure for camps

--- db/0052_stage_052.down.sql
+++ db/0052_stage_052.down.sql
@@ -10,3 +10,10 @@ UPDATE river
 SET props = props - 'vodinfo_sensors' || jsonb_build_object('vodinfo_sensor', props -> 'vodinfo_sensors' -> 0)
 WHERE props ? 'vodinfo_sensors';
 
+--##################################
+DROP TABLE camp_river_ref;
+DROP TABLE camp_rate;
+DROP TABLE rate;
+DROP TABLE camp_photo;
+DROP TABLE photo;
+DROP TABLE camp;
