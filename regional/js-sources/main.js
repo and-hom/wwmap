a8bca1378a9946d1a564ddba2bbc4101f4ef4750
Andrey Khomyakov <and.hom@gmail.com>
2020-05-10 23:28:46 +0300

    #307 WW Guide for single country: web

---
+++ regional/js-sources/main.js
@@ -0,0 +1,8 @@
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+import './style/index.css'
+
+import $ from 'jquery';
+
+global.jQuery = $;
+global.$ = $;
