c078938fe79bf98b1bfd3426085cb5d771dfb427
Andrey Khomyakov <and.hom@gmail.com>
2020-10-28 01:06:49 +0300

    #357 Get linked rivers without spots

--- lib/dao/queries/linked-entity.sql
+++ lib/dao/queries/linked-entity.sql
@@ -6,6 +6,6 @@ SELECT r.id,
        ST_AsGeoJSON(ST_Extent(white_water_rapid.point)) AS bounds
 FROM river r
          INNER JOIN region reg ON r.region_id = reg.id
-         INNER JOIN white_water_rapid ON r.id = white_water_rapid.river_id
+         LEFT OUTER JOIN white_water_rapid ON r.id = white_water_rapid.river_id
 WHERE r.id = ANY($1)
 GROUP BY 1, 2, 3, 4;
