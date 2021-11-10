b494c895def5ead35b335f0272363f02b98aa65b
Andrey Khomyakov <and.hom@gmail.com>
2021-11-10 19:50:12 +0300

    #402 Fix history log

--- lib/dao/queries/changes-log.sql
+++ lib/dao/queries/changes-log.sql
@@ -2,33 +2,43 @@
 changes_log
 
 --@values
-___table___.object_type,
-___table___.object_id,
-___table___.auth_provider,
-___table___.ext_id,
-___table___.login,
-___table___."type",
-___table___.description,
-___table___."time"
+object_type,
+object_id,
+auth_provider,
+ext_id,
+login,
+"type",
+description,
+"time"
 
 --@insert
-INSERT INTO ___table___(___values___) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id
+INSERT INTO ___table___(___values___) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id;
 --@list
-SELECT id, ___values___ FROM ___table___ WHERE object_type=$1 AND object_id=$2 ORDER BY "time" ASC LIMIT $3
+SELECT id, ___values___ FROM ___table___ WHERE object_type=$1 AND object_id=$2 ORDER BY "time" ASC LIMIT $3;
 --@list-all
-SELECT id, ___values___ FROM ___table___ ORDER BY "time" DESC LIMIT $1
+SELECT id, ___values___ FROM ___table___ ORDER BY "time" DESC LIMIT $1;
 --@list-time-range
-SELECT id, ___values___ FROM ___table___ WHERE time>=$1 AND time<$2 ORDER BY "time" DESC LIMIT $3
+SELECT id, ___values___ FROM ___table___ WHERE time>=$1 AND time<$2 ORDER BY "time" DESC LIMIT $3;
 --@list-all-with-user-info
 SELECT changes_log.id, ___values___, COALESCE(info, '{}'::jsonb)
 FROM changes_log
          LEFT OUTER JOIN "user" ON changes_log.auth_provider = "user".auth_provider AND changes_log.ext_id = "user".ext_id
 ORDER BY "time" DESC
-LIMIT $1
+LIMIT $1;
 --@list-with-user-info
-SELECT changes_log.id, ___values___, COALESCE(info, '{}'::jsonb)
+SELECT
+       changes_log.id,
+       changes_log.object_type,
+       changes_log.object_id,
+       changes_log.auth_provider,
+       changes_log.ext_id,
+       changes_log.login,
+       changes_log."type",
+       changes_log.description,
+       changes_log."time",
+       COALESCE("user".info, '{}'::jsonb)
 FROM changes_log
          LEFT OUTER JOIN "user" ON changes_log.auth_provider = "user".auth_provider AND changes_log.ext_id = "user".ext_id
 WHERE object_type=$1 AND object_id=$2
 ORDER BY "time" ASC
-LIMIT $3
+LIMIT $3;
