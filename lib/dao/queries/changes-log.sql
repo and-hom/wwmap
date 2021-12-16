efc10431ba56086eb00e40f87c5fb1c0a3c93500
Andrey Khomyakov <and.hom@gmail.com>
2021-12-16 23:57:34 +0300

    #421 Fix changes log page

--- lib/dao/queries/changes-log.sql
+++ lib/dao/queries/changes-log.sql
@@ -1,24 +1,30 @@
---@table
-changes_log
-
 --@values
-object_type,
-object_id,
-auth_provider,
-ext_id,
-login,
-"type",
-description,
-"time"
+changes_log.object_type,
+changes_log.object_id,
+changes_log.auth_provider,
+changes_log.ext_id,
+changes_log.login,
+changes_log."type",
+changes_log.description,
+changes_log."time"
 
 --@insert
-INSERT INTO ___table___(___values___) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id;
+INSERT INTO changes_log(
+    object_type,
+    object_id,
+    auth_provider,
+    ext_id,
+    login,
+    "type",
+    description,
+    "time"
+) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id;
 --@list
-SELECT id, ___values___ FROM ___table___ WHERE object_type=$1 AND object_id=$2 ORDER BY "time" ASC LIMIT $3;
+SELECT id, ___values___ FROM changes_log WHERE object_type=$1 AND object_id=$2 ORDER BY "time" ASC LIMIT $3;
 --@list-all
-SELECT id, ___values___ FROM ___table___ ORDER BY "time" DESC LIMIT $1;
+SELECT id, ___values___ FROM changes_log ORDER BY "time" DESC LIMIT $1;
 --@list-time-range
-SELECT id, ___values___ FROM ___table___ WHERE time>=$1 AND time<$2 ORDER BY "time" DESC LIMIT $3;
+SELECT id, ___values___ FROM changes_log WHERE time>=$1 AND time<$2 ORDER BY "time" DESC LIMIT $3;
 --@list-all-with-user-info
 SELECT changes_log.id, ___values___, COALESCE(info, '{}'::jsonb)
 FROM changes_log
